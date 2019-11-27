7d2dd9c1818359eda280424967a426dff3f1e0cd
Martin Peterson <peterson_martin@shure.com>
2019-11-27 17:05:57 -0600

    Completed module

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -8,6 +8,6 @@ namespace StarChart.Data
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<CelestialObject > CelastialObjects { get; set; }
+        public DbSet<CelestialObject > CelestialObjects { get; set; }
     }
 }
